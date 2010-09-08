9b78c75a17d4f9cb9daf93426010bb1964f53d82
Mitch <mdempsey@mdempseymac.ics.uci.edu>
2010-09-08 13:38:42 -0700

    Adding more testing data

--- lib/demoman.rb
+++ lib/demoman.rb
@@ -1,13 +1,28 @@
 
 class Demoman
   
+  attr_reader :server_address, :player_name, :map, :game, :demo_protocol, :network_protocol, :type, :duration, :ticks, :frames, :sign_on_length
+  
   def initialize(file)
     io = File.new(file, "r")
     
     data = io.sysread(4096)
     
     
-    puts data.unpack("A8/I/I/A260/A260/A260/A260/f/I/I/I/").inspect
+    demodata = data.unpack("A8/I/I/A260/A260/A260/A260/f/I/I/I/")
+    @type = demodata[0]
+    @demo_protocol = demodata[1]
+    @network_protocol = demodata[2]
+    @server_address = demodata[3]
+    @player_name = demodata[4]
+    @map = demodata[5]
+    @game = demodata[6]
+    
+    @duration = demodata[7]
+    @ticks = demodata[8]
+    @frames = demodata[9]
+    @sign_on_length = demodata[10]
+    
     
   end
   
--- test/test_demoman.rb
+++ test/test_demoman.rb
@@ -5,5 +5,15 @@ class TestDemoman < Test::Unit::TestCase
     demoman = Demoman.new("test/test.dem")
     assert true
     #flunk "hey buddy, you should probably rename this file and start testing for real"
+    
+    assert_equal "69.90.189.79:27037", demoman.server_address
+    assert_equal "Ham Salad[AOF]", demoman.player_name
+    assert_equal "dod_jagd", demoman.map
+    assert_equal "dod", demoman.game
+    assert_equal 3, demoman.demo_protocol
+    assert_equal 7, demoman.network_protocol
+    
+    #["HL2DEMO", 3, 7, "69.90.189.79:27037", "Ham Salad[AOF]", "dod_jagd", "dod", 13.2424249649048, 874, 390, 115449]
+    
   end
 end
